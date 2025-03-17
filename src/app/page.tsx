ffe2d93cb6ae73f808b7fa5a9ebdfbe3cd653d8a
Roberto Calvo <rcalvo.retana@gmail.com>
2025-03-17 16:39:57 -0600

    feat: Mostrar todos los servicios en la página principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
       <Hero />
-      <ServicesSection />
+      <ServicesSection showAllServices={true} />
       <TrustSection />
       <CasesSection />
       <BlogSection />
